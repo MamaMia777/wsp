2113411a2ab0ca91360238c0a7674e87f3700451
Rolands Polovnuks <mamamia777@Rolandss-MacBook-Pro.local>
2023-12-22 17:25:44 +0100

    dd

--- back/src/app/auth/auth.service.ts
+++ back/src/app/auth/auth.service.ts
@@ -17,8 +17,8 @@ export class AuthService {
     this.oauthClient = new google.auth.OAuth2(
       process.env.GOOGLE_CLIENT_ID,
       process.env.GOOGLE_SECRET,
-      'http://localhost:3000/auth/google/callback',
-      // 'https://wsp.company/auth/google/callback',
+      // 'http://localhost:3000/auth/google/callback',
+      'https://wsp.company/auth/google/callback',
     );
   }
 
